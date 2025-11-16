63d3d05a3415e06cbe8dc9ee5fb474066a37d5dd
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-16 16:37:07 +0530

    Converting String To Uppercase and Lowercase

--- String.dart
+++ String.dart
@@ -14,5 +14,23 @@ void main() {
   print("Using +, Full Name is "+firstName + " " + lastName+".");
   print("Using interpolation, full name is $firstName $lastName.");
   
+  //String Properties
+  print("-----------------------String Properties------------------------");
+  String str = "Hi";
+  print(str.codeUnits);   //Example of code units
+  print(str.isEmpty);     //Example of isEmpty
+  print(str.isNotEmpty);  //Example of isNotEmpty
+  print("The length of the string is: ${str.length}");   //Example of Length
+
+  //Converting String To Uppercase and Lowercase
+  print("-----------------------------Converting String To Uppercase and Lowercase-------------------");
+  String address1 = "Florida"; // Here F is capital
+  String address2 = "TexAs"; // Here T and A are capital
+  print("Address 1 in uppercase: ${address1.toUpperCase()}"); 
+  print("Address 1 in lowercase: ${address1.toLowerCase()}"); 
+  print("Address 2 in uppercase: ${address2.toUpperCase()}"); 
+  print("Address 2 in lowercase: ${address2.toLowerCase()}"); 
   
+
+
 }
