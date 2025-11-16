87830f0a4883af2b2c871b364797df3221ab6d52
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-16 16:30:31 +0530

    String Concatenation In Dart

---
+++ String.dart
@@ -0,0 +1,18 @@
+void main() {   
+
+   String text1 = 'This is an example of a single-line string.';   
+   String text2 = "This is an example of a single line string using double quotes.";   
+   String text3 = """This is a multiline line string using the triple-quotes.This is tutorial on dart strings.""";   
+   print(text1);  
+   print(text2);   
+   print(text3);   
+  
+  //String Concatenation In Dart
+  print("------------------------String Concatenation In Dart--------------------");
+  String firstName = "John";
+  String lastName = "Doe";
+  print("Using +, Full Name is "+firstName + " " + lastName+".");
+  print("Using interpolation, full name is $firstName $lastName.");
+  
+  
+}
