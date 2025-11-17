fee475f075318d2d8d59467a15f7272d4dd1966c
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-17 22:50:28 +0530

    Map Properties

--- Map.dart
+++ Map.dart
@@ -18,4 +18,21 @@ void main(){
   };
   print(countryCapital2["USA"]);
 
+  //Map Properties
+  print("-----------------------------------Map Properties----------------------------------");
+  Map<String, double> expenses = {
+    'sun': 3000.0,
+    'mon': 3000.0,
+    'tue': 3234.0,
+  };
+  
+  print("All keys of Map: ${expenses.keys}");
+  print("All values of Map: ${expenses.values}");
+  print("Is Map empty: ${expenses.isEmpty}");
+  print("Is Map not empty: ${expenses.isNotEmpty}");
+  print("Length of map is: ${expenses.length}");
+
+  
+
+
 }
