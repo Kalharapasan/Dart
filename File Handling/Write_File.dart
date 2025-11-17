b5582db1ebf4511ed4cbcf0632f1a99a61dc8dad
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-18 00:26:44 +0530

    Add New Content To Previous Content

--- File Handling/Write_File.dart
+++ File Handling/Write_File.dart
@@ -11,7 +11,13 @@ void main() {
   file.writeAsStringSync('Welcome to test.txt file.');
   print('File written.');
 
-  
+  //Add New Content To Previous Content
+  print("---------------------------------------Add New Content To Previous Content----------------------------------------");
+  // open file
+  File file2 =  File('test.txt');
+  // write to file
+  file2.writeAsStringSync('\nThis is a new content.', mode: FileMode.append);
+  print('Congratulations!! New content is added on top of previous content.');
 
 
 }
