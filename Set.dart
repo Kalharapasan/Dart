cbb083e3a98ced3eefb625225fa4734698ead081
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-17 22:03:08 +0530

    Create A Set In Dart

---
+++ Set.dart
@@ -0,0 +1,9 @@
+void main(){
+  //Create A Set In Dart
+  print("----------------------------------Create A Set In Dart-----------------------------------");
+  Set<String> fruits = {"Apple", "Orange", "Mango"};
+  print(fruits);
+
+  
+
+}
