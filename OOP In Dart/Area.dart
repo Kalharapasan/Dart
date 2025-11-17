1c67f11fecba216423e62f3fc3aef84f335724d9
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-18 00:56:39 +0530

    Find Area Of Ractangle Using Class and Objects

--- OOP In Dart/Area.dart
+++ OOP In Dart/Area.dart
@@ -1,8 +1,26 @@
-class Area {
+class Rectangle{
+  //properties of rectangle
   double? length;
   double? breadth;
-
-  double calculateArea() {
+  
+  //functions of rectangle
+  double area(){
     return length! * breadth!;
   }
+}
+
+//Find Area Of Ractangle Using Class and Objects
+void main(){
+  print("----------------------------------Find Area Of Ractangle Using Class and Objects------------------------");
+  //object of rectangle created
+  Rectangle rectangle = Rectangle();
+  
+  //setting properties for rectangle
+  rectangle.length=10;
+  rectangle.breadth=5;
+  
+  //functions of rectangle called
+  print("Area of rectangle is ${rectangle.area()}.");
+
+  
 }
