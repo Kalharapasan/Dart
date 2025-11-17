624ac9b6c47bff270a6e8abc0cfb44972df2a577
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-18 00:30:04 +0530

    Write CSV File In Dart

--- File Handling/Write_File.dart
+++ File Handling/Write_File.dart
@@ -19,5 +19,24 @@ void main() {
   file2.writeAsStringSync('\nThis is a new content.', mode: FileMode.append);
   print('Congratulations!! New content is added on top of previous content.');
 
+  //Write CSV File In Dart
+  print("---------------------------------------AWrite CSV File In Dart----------------------------------------");
+  // open file
+  File file3 = File("students.csv");
+  // write to file
+  file3.writeAsStringSync('Name,Phone\n');
+  for (int i = 0; i < 3; i++) {
+    // user input name
+    stdout.write("Enter name of student ${i + 1}: ");
+    String? name = stdin.readLineSync();
+    stdout.write("Enter phone of student ${i + 1}: ");
+    // user input phone
+    String? phone = stdin.readLineSync();
+    file3.writeAsStringSync('$name,$phone\n', mode: FileMode.append);
+  }
+  print("Congratulations!! CSV file written successfully.");
+
+  
+
 
 }
