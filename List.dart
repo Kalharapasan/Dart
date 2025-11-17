08d15f2b729ba13fa28d25292d1b14634cdf25e7
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-17 21:27:55 +0530

    Growable List

--- List.dart
+++ List.dart
@@ -8,6 +8,14 @@ List<String> names = ["Raj", "John", "Rocky"];
 var mixed = [10, "John", 18.8];
 
 void main() {  
-   var list = List<int>.filled(5,0);  
-   print(list);  
+  //Fixed Length List
+  print("-------------------------------------Fixed Length List------------------------------------------------");
+  var list = List<int>.filled(5,0);  
+  print(list);
+
+  //Growable List
+  print("-------------------------------------Growable List------------------------------------------------");
+  var list1 = [210,21,22,33,44,55];  
+  print(list1); 
+
 }
