9d4b4d1bb6d8ec3e9a3c1d403339af7a0aac109d
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-17 21:26:29 +0530

    Fixed Length List

---
+++ List.dart
@@ -0,0 +1,13 @@
+// Integer List
+List<int> ages = [10, 30, 23];
+
+// String List
+List<String> names = ["Raj", "John", "Rocky"];
+
+// Mixed List
+var mixed = [10, "John", 18.8];
+
+void main() {  
+   var list = List<int>.filled(5,0);  
+   print(list);  
+}
