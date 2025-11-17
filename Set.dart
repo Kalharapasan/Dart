68fccfbe5f096ae835ed670e05e62efc715d0ec2
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-17 22:04:23 +0530

    Set Properties

--- Set.dart
+++ Set.dart
@@ -4,6 +4,18 @@ void main(){
   Set<String> fruits = {"Apple", "Orange", "Mango"};
   print(fruits);
 
+  //Set Properties
+  print("----------------------------------Set Properties-----------------------------------");
+  // declaring fruits as Set
+  Set<String> fruits2 = {"Apple", "Orange", "Mango", "Banana"};
+
+  // using different properties of Set
+  print("First Value is ${fruits2.first}");
+  print("Last Value is ${fruits2.last}");
+  print("Is fruits empty? ${fruits2.isEmpty}");
+  print("Is fruits not empty? ${fruits2.isNotEmpty}");
+  print("The length of fruits is ${fruits2.length}");
+
   
 
 }
