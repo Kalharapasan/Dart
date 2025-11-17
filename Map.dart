a496cb912c9d93a0935d20f3840e76421285a9c2
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-17 22:48:14 +0530

    Create Map In Dart

---
+++ Map.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  //Create Map In Dart
+  print("-----------------------------------Create Map In Dart----------------------------------");
+  Map<String, String> countryCapital = {
+    'USA': 'Washington, D.C.',
+    'India': 'New Delhi',
+    'China': 'Beijing'
+  };
+  print(countryCapital);
+}
